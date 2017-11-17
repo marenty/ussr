3723348db74214363b05ebe4f68d215aaef02150
marenty <marekruc@yahoo.pl>
2017-11-17 20:32:23 +0100

    Dodana strona about oraz kontakt

--- MainPage/urls.py
+++ MainPage/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'MainPage'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^about/', views.about, name='about')
     ]
--- MainPage/views.py
+++ MainPage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from company.models import CompanyBranch, CompanyDescription
@@ -15,3 +15,18 @@ def index(request):
                 'shortdescription': shortdescription}
 
     return render(request, 'MainPage/index.html', context)
+
+
+def contact(request):
+
+    branches_list = get_list_or_404(CompanyBranch)
+    context = {'branches_list': branches_list}
+
+    return render(request, 'MainPage/contact.html', context)
+
+def about(request):
+
+    description = get_object_or_404(CompanyDescription, id_company_description = 'Witam')
+    context = {'description': description}
+
+    return render(request, 'MainPage/about.html', context)
