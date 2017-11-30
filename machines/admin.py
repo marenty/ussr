d7efab3c340016bd7e33b2647e33160d89bd1a15
ugkontzal <row.pat11@interia.pl>
2017-11-30 13:59:50 +0100

    zmiany w panelu adm maszyny i typy maszyn

--- machines/admin.py
+++ machines/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
-admin.site.register(Machine)
-admin.site.register(MachineType)
+
+
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ('id_machine', 'machine_name', 'machine_type', 'service_interval', 'last_service', 'is_operational', 'notes', 'company_branch')
+    list_filter = ('machine_type', 'machine_name', 'service_interval', 'is_operational')
+    #search_fields = ('machine_type', 'machine_name')
+    #prepopulated_fields = {'slug': ('title',)}
+    #raw_id_fields = ('author',)
+    #date_hierarchy = 'publish'
+    #ordering = ['status', 'publish']
+
+admin.site.register(Machine, MachineAdmin)
+
+class MachineTypeAdmin(admin.ModelAdmin):
+    list_display = ('machine_type_name', 'id_machine_type')
+    list_filter = ('machine_type_name', 'id_machine_type')
+    #search_fields = ('machine_type_name')
+    #prepopulated_fields = {'slug': ('title',)}
+    #raw_id_fields = ('author',)
+    #date_hierarchy = 'publish'
+    #ordering = ['status', 'publish']
+
+admin.site.register(MachineType, MachineTypeAdmin)
+
+
+#admin.site.register(Machine)
+#admin.site.register(MachineType)
